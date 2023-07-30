b369d0677e8eabaf5129c86c91281c156842f0f4
Kyle <kylesch115@gmail.com>
2023-07-30 11:01:02 -0600

    setup first endpoint to get db data

--- src/functions/test/index.ts
+++ src/functions/test/index.ts
@@ -1,23 +1,14 @@
-// import { APIGatewayProxyEvent } from "aws-lambda";
-// import { getTestTableItems } from "./queries";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { getTestTableItems } from "./queries";
+import { buildResponse } from "../../helpers/buildResponse";
 
-module.exports.getTestTable = async (event: any) => {
-    console.log("wego")
+module.exports.getTestTable = async (event: APIGatewayProxyEvent) => {
     console.log("event\n", event);
     try {
-        // const response = await getTestTableItems();
-        return {
-            statusCode: 200,
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-            },
-            body: JSON.stringify({
-                msg: "hello-"
-            }),
-        };
+        const response = await getTestTableItems();
+        return buildResponse(200, { response });
     } catch (error) {
         console.log("error\n", error);
+        return buildResponse(500, { message: "error getting TestTable items" });
     }
 };
