9124c005cd4c74d52cc030d2e4c5de6928b191ad
Kyle <kylesch115@gmail.com>
2023-07-30 10:56:12 -0600

    establish first basic api endpoint

--- src/functions/test/index.ts
+++ src/functions/test/index.ts
@@ -1,8 +1,11 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
+// import { APIGatewayProxyEvent } from "aws-lambda";
+// import { getTestTableItems } from "./queries";
 
-module.exports.getTestTable = async (event: APIGatewayProxyEvent) => {
+module.exports.getTestTable = async (event: any) => {
+    console.log("wego")
     console.log("event\n", event);
     try {
+        // const response = await getTestTableItems();
         return {
             statusCode: 200,
             headers: {
@@ -10,11 +13,11 @@ module.exports.getTestTable = async (event: APIGatewayProxyEvent) => {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": true,
             },
-            body: {
+            body: JSON.stringify({
                 msg: "hello-"
-            },
+            }),
         };
     } catch (error) {
         console.log("error\n", error);
     }
-}
+};
