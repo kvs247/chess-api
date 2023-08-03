a3ffe49f03ffb2e76f337a624556f87b65eb9746
Kyle <kylesch115@gmail.com>
2023-08-02 22:38:44 -0600

    save new game state to database

--- src/functions/move/index.ts
+++ src/functions/move/index.ts
@@ -1,17 +1,23 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { buildResponse } from "../../helpers/buildResponse";
 import { MoveRequest } from "./types";
-
-import { processMove } from "./queries";
+import { getFenFromMove } from "../../helpers/chess/helpers";
+import { putGame } from "./queries";
 
 module.exports.move = async (event: APIGatewayProxyEvent) => {
   console.log("event:\n", event);
   try {
     if (event.body) {
       const body: MoveRequest = JSON.parse(event.body);
-      const { fen, toIndex, fromIndex } = body;
-      const response = await processMove({ fen, toIndex, fromIndex });
-      return buildResponse(200, { response });
+      // const body = event.body;
+      const { fen, fromIndex, toIndex } = body;
+      const newFen = getFenFromMove(fen, fromIndex, toIndex);
+      const newGameState = {
+        gameId: "0",
+        fen: newFen,
+      };
+      await putGame(newGameState);
+      return buildResponse(200, { newFen });
     } else {
       const message = "No body in HTTP request";
       console.log("Error:\n", message);
--- src/functions/move/queries.ts
+++ src/functions/move/queries.ts
@@ -1,9 +1,22 @@
-import { MoveRequest } from "./types";
+import AWS from "aws-sdk";
+import { queryTable } from "../../helpers/baseQueries";
+import { buildResponse } from "../../helpers/buildResponse";
 
-import { processInput } from "./helpers";
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-export async function processMove(payload: MoveRequest) {
-  const { fen, fromIndex, toIndex  } = payload;
-  const result = processInput(fen, fromIndex, toIndex);
-  return result;
+const dynamoDBTableName = "Games-dev";
+
+export async function putGame(gameData: any) {
+  const params = {
+    TableName: dynamoDBTableName,
+    Item: gameData,
+  };
+  return await dynamoDB
+    .put(params)
+    .promise()
+    .then(() => {
+      return buildResponse(201, {});
+    }, (error) => {
+      console.log("Error saving new game state:\n", error);
+    });
 };
