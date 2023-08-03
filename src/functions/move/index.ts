8993775d8b31755a9ecd45c3b967fa6fea1bcb4c
Kyle <kylesch115@gmail.com>
2023-08-02 23:35:34 -0600

    add /getGameById endpoint

--- src/functions/move/index.ts
+++ src/functions/move/index.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { buildResponse } from "../../helpers/buildResponse";
-import { MoveRequest } from "./types";
+import { ProcessMoveRequest } from "./types";
 import { getFenFromMove } from "../../helpers/chess/helpers";
-import { putGame } from "./queries";
+import { getGameById, putGame } from "./queries";
 
-module.exports.move = async (event: APIGatewayProxyEvent) => {
+module.exports.processMove = async (event: APIGatewayProxyEvent) => {
   console.log("event:\n", event);
   try {
     if (event.body) {
-      const body: MoveRequest = JSON.parse(event.body);
+      const body: ProcessMoveRequest = JSON.parse(event.body);
       // const body = event.body;
       const { fen, fromIndex, toIndex } = body;
       const newFen = getFenFromMove(fen, fromIndex, toIndex);
@@ -26,5 +26,25 @@ module.exports.move = async (event: APIGatewayProxyEvent) => {
   } catch (error) {
     console.log("error:\n", error);
     return buildResponse(500, { message: "error processing move" });
+  }
+};
+
+module.exports.getGameById = async (event: APIGatewayProxyEvent) => {
+  console.log("event:\n", event);
+  try {
+    if (
+      event.pathParameters &&
+      event.pathParameters.id
+    ) {
+      const gameId = event.pathParameters.id;
+      const [game] = await getGameById(gameId)
+      return buildResponse(200, { game });
+    } else {
+      const message = "event does not have body";
+      console.log("error:\n", message);
+      return buildResponse(400, { message });
+    }
+  } catch (error) {
+    return buildResponse(500, { message: "error getting game" });
   }
 };
--- src/functions/move/queries.ts
+++ src/functions/move/queries.ts
@@ -6,6 +6,18 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 const dynamoDBTableName = "Games-dev";
 
+export async function getGameById(gameId: string) {
+  const params = {
+    TableName: dynamoDBTableName,
+    KeyConditionExpression: "gameId = :gameId",
+    ExpressionAttributeValues: {
+      ":gameId": gameId,
+    },
+  };
+  const response = await queryTable(params);
+  return response;
+};
+
 export async function putGame(gameData: any) {
   const params = {
     TableName: dynamoDBTableName,
